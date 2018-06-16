489a933a6b51d0f6217eda67d9074cd94d98bf81
Ben Holmes <ben@coolship.io>
2018-06-16 18:23:03 +0000

    2nd commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='dd-python',
+      version='1.0',
+      description='dnamic-dev python tools',
+      author='Ben Holmes',
+      author_email='ben@coolship.io',
+      url='http://dnamicroscopy.org',
+      install_requires=["ipython<6"],
+
+)
